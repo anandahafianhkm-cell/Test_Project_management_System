84b6ee751f8f53a1a33590286998b9af81c601fb
anandahafianhkm@gmail.com <anandahafianhkm@gmail.com>
2026-08-24 21:17:23 +0700

    penerusan project

--- js/pajak5Tahunan.js
+++ js/pajak5Tahunan.js
@@ -11,4 +11,19 @@ form.addEventListener('submit', function (event) {
     }
 
     const tanggalMasuk = document.getElementById('tanggal_masuk').value;
+    const nomorPolisi = document.getElementById('nomor_polisi').value;
+    const totalBiaya = parseFloat(document.getElementById('total_biaya').value) || 0;
+    const DP = parseFloat(document.getElementById('dp_dibayar').value) || 0;
+    const kekuranganBiaya = parseFloat(document.getElementById('kekurangan_biaya').value) || 0;
+    const kelengkapanBerkas = {
+        "bpkb_asli": document.getElementById('bpkb-asli').checked,
+        "stnk_asli": document.getElementById('stnk-asli').checked,
+        "ktp_asli": document.getElementById('ktp-asli').checked,
+        "bpkb-fc": document.getElementById('bpkb-fc').checked,
+        "stnk_fc": document.getElementById('stnk-fc').checked,
+        "ktp_fc": document.getElementById('ktp-fc').checked
+    }
+
+    const mitraPengerjaan = document.getElementById('mitra').value;
+    const catatan = document.getElementById('catatan').value;
 })
