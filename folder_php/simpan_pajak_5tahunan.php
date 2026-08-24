67eb84355810856cfdb4d7106bbbd0660c3e97a2
anandahafianhkm@gmail.com <anandahafianhkm@gmail.com>
2026-08-24 22:21:39 +0700

    penerusan project

--- folder_php/simpan_pajak_5tahunan.php
+++ folder_php/simpan_pajak_5tahunan.php
@@ -6,5 +6,27 @@ $json = file_get_contents("php://input");
 $data = json_decode($json, true);
 
 if ($data) {
-    
+    $nama = $conn->real_escape_string($data['nama_konsumen']);
+    $tanggal = $conn->real_escape_string($data['tanggal_masuk']);
+    $plat = $conn->real_escape_string($data['nomor_polisi']);
+    $total = $conn->real_escape_string($data['total_biaya']);
+    $dp = $conn->real_escape_string($data['dp_dibayar']);
+    $kekurangan = $conn->real_escape_string(($data['kekurangan_biaya']));
+    $berkas = $conn->real_escape_string($data['kelengkapan_berkas']);
+    $mitra = $conn->real_escape_string($data['mitra_pengerjaan']);
+    $catatan = $conn->real_escape_string($data['catatan']);
+
+    $sql = "INSERT INTO pajak_5tahunan
+            (nama_konsumen, tanggal_masuk, nomor_polisi, total_biaya, dp_dibayar, kekurangan_biaya, kelengkapan_berkas, mitra_pengerjaan, catatan)
+            VALUES
+            ('$nama', '$tanggal', '$plat', '$total', '$dp', '$kekurangan', '$berkas', '$mitra', '$catatan')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo json_encode(["status" => "success", "message" => "Data Pajak 5 Tahunan Berhasil Disimpan"]);
+    } else {
+        echo json_encode(["status" => "error", "message" => "Gagal Simpan: " . $conn->error]);
+    }
+} else {
+    echo json_encode(["status" => "error", "message" => "Data JSON tidak valid"]);
 }
+?>
